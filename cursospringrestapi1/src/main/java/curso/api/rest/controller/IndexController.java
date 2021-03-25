a18e52db642b4689278efe2e625ae1aa52f6911b
Luis Carlos <luiscarlos8pe@gmail.com>
2021-03-25 14:55:59 -0300

    Criando paramento nome

--- cursospringrestapi1/src/main/java/curso/api/rest/controller/IndexController.java
+++ cursospringrestapi1/src/main/java/curso/api/rest/controller/IndexController.java
@@ -4,16 +4,19 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@RestController/*arquitetura REST*/
 @RequestMapping(value = "/usuario")
 public class IndexController {
 	
-	
+	/*serviço RESTful*/
 	@GetMapping(value = "/", produces = "application/json")
-	public ResponseEntity init() {
-		return new ResponseEntity("Ola usuario / spring boot filho da puta", HttpStatus.OK);
+	public ResponseEntity init(@RequestParam (value="nome",  required = true, defaultValue = "Nome obrigatorio") String nome) {
+		System.out.println("Parametro sendo recebido " + nome);
+		return new ResponseEntity("Ola usuario / spring boot seu nome e: " + nome, HttpStatus.OK);
 	}
+	
 
 }
